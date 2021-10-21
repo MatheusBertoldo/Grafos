ec60248257047b06a44757e1eb474191f7328b0c
MatheusBertoldo <52037968+MatheusBertoldo@users.noreply.github.com>
2021-10-21 08:24:52 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,124 +1,127 @@
 import java.io.*;
 import java.util.*;
+enum Color {BLACK, GREY, WHITE;}
 class Graph {
     private int V;
     private LinkedList<Integer>[] adj;
+    private Color[] visited;
+    public int[] distance;
+    private Integer[] parentNode;
+    public String path = "";
     public Graph(int v) {
         V = v;
+        parentNode = new Integer[v];
+        visited = new Color[v];
+        distance = new int[v];
         adj = new LinkedList[v];
-        for (int i=0; i<v; ++i) adj[i] = new LinkedList();
+        for (int i = 0; i < v; ++i) adj[i] = new LinkedList();
     }
-    int getVertices(){
-        return V;
+    int getVertices() {
+        return this.V;
     }
-    int getMax(){
+    int getMax() {
         int max_degree = Integer.MIN_VALUE;
-        for(int i = 0; i < adj.length; i++){
-            if(adj[i].size() > max_degree) max_degree = adj[i].size();
+        for (int i = 0; i < adj.length; i++) {
+            if (adj[i].size() > max_degree) max_degree = adj[i].size();
         }
         return max_degree;
     }
-    int getMin(){
+    int getMin() {
         int min_degree = Integer.MAX_VALUE;
-        for(int i = 0; i < adj.length; i++){
-            if(adj[i].size() < min_degree) min_degree = adj[i].size();
+        for (int i = 0; i < adj.length; i++) {
+            if (adj[i].size() < min_degree) min_degree = adj[i].size();
         }
         return min_degree;
     }
-    double getAverage(){
-        return (double) (getMax()+getMin())/2;
+    double getAverage() {
+        int sum = 0;
+        for(int i = 0; i < getVertices(); i++){
+            sum += adj[i].size();
+        }
+        return (double) sum/getVertices();
     }
     void addEdge(int v, int w) {
-        adj[v-1].add(w-1);
-        adj[w-1].add(v-1);
+        adj[v - 1].add(w - 1);
+        adj[w - 1].add(v - 1);
     }
-    void DFSUtil(int v, boolean[] visited) {
-        visited[v] = true;
-        Iterator<Integer> i = adj[v].listIterator();
-        while (i.hasNext()) {
-            int n = i.next();
-            if (!visited[n]) DFSUtil(n, visited);
-        }
-    }
-    boolean isConnected() {
-        boolean[] visited = new boolean[V];
-        int i;
-        for (i = 0; i < V; i++) visited[i] = false;
-        for (i = 0; i < V; i++) {
-            if (adj[i].size() != 0) break;
-        }
-        if (i == V) return true;
-        DFSUtil(i, visited);
-        for (i = 0; i < V; i++) {
-            if (!visited[i] && adj[i].size() > 0) return false;
-        }
-        return true;
-    }
-    int isEulerian() {
-        if (!isConnected()) return 0;
-        int odd = 0;
-        for (int i = 0; i < V; i++) {
-            if (adj[i].size() % 2 != 0) odd++;
-        }
-        if (odd > 2) return 0;
-        return (odd == 2)? 1 : 2;
-    }
-    void BFS(int s) {
-        s = s-1;
-        boolean visited[] = new boolean[V];
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        visited[s] = true;
-        queue.add(s);
-        while (queue.size() != 0) {
-            s = queue.poll();
-            System.out.print((s+1) + " ");
-            Iterator<Integer> i = adj[s].listIterator();
-            while (i.hasNext()) {
-                int n = i.next();
-                if (!visited[n]) {
-                    visited[n] = true;
-                    queue.add(n);
+    void BFS(int root){
+        Arrays.fill(visited, Color.WHITE); Arrays.fill(distance, -1); Arrays.fill(parentNode, null);
+        visited[root] = Color.GREY;
+        distance[root] = 0;
+        parentNode[root] = null;
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            root = queue.poll();
+            System.out.print((root+1) + " ");
+            for(Integer i : adj[root]){
+                if(visited[i] == Color.WHITE){
+                    visited[i] = Color.GREY;
+                    distance[i] = distance[root] + 1;
+                    parentNode[i] = root;
+                    queue.add(i);
                 }
-            }
+            } visited[root] = Color.BLACK;
         }
     }
-    void test() {
-        int res = isEulerian();
-        if (res == 0)
-            System.out.println("O grafo não é Euleriano");
-        else if (res == 1)
-            System.out.println("O grafo é semi-Euleriano");
-        else
-            System.out.println("O grafo é Euleriano");
+    void createPath(int start, int target){
+        if(target == start) path += (start+1) + " ";
+        else if(parentNode[target] == null) System.out.printf("Nenhum caminho existente.%n");
+        else{
+            createPath(start, parentNode[target]);
+            path += (target+1) + " ";
+        }
     }
 }
-public class Main{
-    public static void main(String args[]) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Escreva o caminho para o arquivo: ");
-        String filePath = reader.readLine();
-        File graphFile = new File(filePath);
-        BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(graphFile)));
+public class Main {
+    static Graph graph;
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static int read_File(File file) throws Exception {
+        BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
         int vertex_count = Integer.parseInt(fileReader.readLine()), edge_count = 0;
-        Graph graph = new Graph(vertex_count);
+        graph = new Graph(vertex_count);
         do{
             String[] edges = fileReader.readLine().split(" ");
             graph.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[1]));
             edge_count++;
-        } while(fileReader.ready());
-        File newFile = new File(graphFile.getPath().replace(graphFile.getName(), "output.txt"));
-        if(!newFile.createNewFile()){
-            System.out.println("Arquivo já existe na pasta de origem, deletando e criando outro...");
-            newFile.delete();
-            newFile.createNewFile();
+        }while(fileReader.ready());
+        fileReader.close();
+        return edge_count;
+    }
+    public static void main(String args[]) throws Exception{
+        System.out.printf("Insira o caminho para o arquivo:%n");
+        String filePath = reader.readLine();
+        if(System.getProperty("os.name").toLowerCase().contains("win")) filePath.replace("\\", "\\\\");
+        File file = new File(filePath);
+        int edge_Count = read_File(file);
+
+        File newFile = new File(file.getPath().replace(file.getName(), "output.txt"));
+        while(true){
+            if(newFile.createNewFile()){
+                System.out.printf("Arquivo criado com êxito!%n");
+                break;
+            }
+            else {
+                System.out.printf("O arquivo %s já existe!%nDeletando...%n", newFile.getName());
+                newFile.delete();
+            }
         }
-        System.out.println("Arquivo criado com êxito!");
-        FileWriter fileWriter = new FileWriter(newFile);
-        fileWriter.write("Número de vértices: "+graph.getVertices()+"\nNúmero de arestas: "+edge_count+"\nGrau máximo: "+graph.getMax()+"\nGrau mínimo: "+graph.getMin()+
+
+        FileWriter writer = new FileWriter(newFile);
+        writer.write("Número de vértices: "+graph.getVertices()+"\nNúmero de arestas: "+edge_Count+"\nGrau máximo: "+graph.getMax()+"\nGrau mínimo: "+graph.getMin()+
                 "\nGrau médio: "+graph.getAverage()+"\n");
-        fileWriter.close();
-        System.out.print("Insira o vértice inicial da busca: ");
-        int initial_vertex = Integer.parseInt(reader.readLine());
-        graph.BFS(initial_vertex);
+
+        System.out.printf("Insira o vértice inicial para a busca:%n");
+        int root = Integer.parseInt(reader.readLine()) - 1;
+        graph.BFS(root);
+        for(int i = 0; i < graph.getVertices(); i++){
+            graph.createPath(root, i);
+            writer.write("Caminho da raiz até o alvo "+(i+1)+": "+graph.path+" com distância de "+graph.distance[i]+"\n");
+            graph.path = "";
+        }
+        writer.write("\n\nNão consegui usar nenhuma API para plottar o grafo :(");
+        System.out.printf("%nSucesso!%n");
+        writer.close();
+        reader.close();
     }
 }
